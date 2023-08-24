29378ae33bf2dad1510b49555e09397ddafc9fe4
Tara025 <tara_jones@web.de>
2023-08-24 11:54:37 +0200

    no push

--- frontend/src/components/DashboardUser.jsx
+++ frontend/src/components/DashboardUser.jsx
@@ -5,6 +5,7 @@ function DashboardUser() {
   return (
     <div className="dashboard-container">
       <h1>Welcome to Your Dashboard</h1>
+     
       <div className="button-container">
         <Link to="/login" className="auth-button">
           Login
